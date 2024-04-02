9926a40718f4aa55b4c080be624a607d2149a586
Manoj Beevanapalli <manojbeevanapalli24@gmail.com>
2024-04-01 23:25:11 -0500

    Added inheritance

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,2 +1,46 @@
-public class i {
+// Parent class (superclass)
+class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
 }
+
+// Child class (subclass) inheriting from Animal
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name); // Call the superclass constructor
+    }
+
+    // Additional method specific to Dog class
+    public void bark() {
+        System.out.println(name + " is barking.");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        // Create a Dog object
+        Dog dog = new Dog("Buddy");
+
+        // Call methods inherited from Animal class
+        dog.eat();   // Inherited from Animal
+        dog.sleep(); // Inherited from Animal
+
+        // Call method specific to Dog class
+        dog.bark();  // Specific to Dog
+
+        // Accessing the 'name' field inherited from Animal class
+        System.out.println("Name of the dog: " + dog.name);
+    }
+}
+
